b9484fd976ef075d8df7c657768dfd0bf1cfdcaf
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-22 04:20:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def batch_badge_creator(array)
 end 
 
 def assign_rooms(array)
-  array.map_with_index do |name, index| 
+  array.each_with_index do |name, index| 
     puts "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end 
 end 
