8b57c32796283308f4ef89276e7d0ebe28fe80d2
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-22 04:33:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,6 +22,7 @@ end
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge  
+  end 
   assign_rooms(attendees).each do |attendee_room|
     puts attendee_room 
 end 
